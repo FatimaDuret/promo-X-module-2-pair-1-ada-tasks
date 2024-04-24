6b30d17e8d98b019538925c61cde33a1236608ce
FatimaDuret <f.duret.g@gmail.com>
2024-04-24 09:55:45 +0200

    adatask condicional

--- js/main.js
+++ js/main.js
@@ -6,61 +6,33 @@ const buttonAdd = document.querySelector('.js-btn-add');
 const inputFilter = document.querySelector('.js-text-task-filter');
 const buttonFilter = document.querySelector('.js-btn-filter'); 
 
+
+// creo esta constante para poder acceder a la lista de tareas
 let classList = listElement.classList;
 
 const tasks = [
     { name: 'Recoger setas en el campo', completed: true },
     { name: 'Comprar pilas', completed: true },
     { name: 'Poner una lavadora de blancos', completed: true },
-    {
-      name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
-      completed: false,
-    },
+    { name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
+      completed: false},
   ];
 
-
+  // creamos esta función para añadir tareas a la lista
   const addTask = (tasks) => {
 
     for (const task of tasks) {
     
-    listElement.innerHTML += `<li class="tachado"> <input type ="checkbox"/>${tasks.name}</li>`;
+    listElement.innerHTML += `<li> <input type ="checkbox"/>${task.name}</li>`;
   };
 
 };
 
   addTask(tasks);
 
-  function renderTask(tasks) {
-    let listElement = '';
-
-
-  if (tasks.completed === true){
-    listElement=  `<li class="tachado"> <input type ="checkbox"/>${tasks.name}</li>`;
-    
-  }else if(tasks.completed === false){
-    listElement= `<li> <input type ="checkbox"/>${tasks.name}</li>`;
-
-  
-  }else{
-    listElement= `<li> <input type ="checkbox"/>${tasks.name}</li>`;
-
-  }
-  return listElement;
-
-
-  }
-  renderTask();
-
-/* 
-  handleRenderTask = (event) => {
+function renderTask(task) {}
    
-  }; */
-  
-  
-  
-  
-  
-  listElement.addEventListener('click', handleClick);
+
 
   
   
@@ -74,4 +46,4 @@ const tasks = [
 
   
 
-  console.log(handleClick);
+  
